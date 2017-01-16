2be1016f42fe87e80526f2a3215d8f37f8356e0e
Mike <mikemel@hotmail.gr>
2017-01-16 10:23:06 +0200

    Starting Point

---
+++ src/Client.java
@@ -0,0 +1,167 @@
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.*;
+import java.security.*;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class Client {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, 
+NoSuchPaddingException, InvalidKeyException, 
+IllegalBlockSizeException, BadPaddingException, 
+InvalidAlgorithmParameterException, ClassNotFoundException {
+
+        Socket mySocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            mySocket = new Socket("localhost", 44430);
+            out = new PrintWriter(mySocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: localhost.");
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String fromServer = null;
+        String fromUser;
+        
+        System.out.println("Client run ");
+        
+        String cryptoaes3 = in.readLine();
+            
+            System.out.println("Download Key: " + cryptoaes3);
+
+            // Decode Key
+            byte[] cryptoaes = Base64.getDecoder().decode(cryptoaes3);
+            System.out.println("Decode Key: " + cryptoaes);
+            
+            // Decrypt AES Key with Client's Private Key
+            ObjectInputStream inputStream = null;
+            inputStream = new ObjectInputStream(new FileInputStream(PRIVATE_KEY_FILE));
+            final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
+            final String uncryaes = decrypt(cryptoaes, privateKey);
+            System.out.println("Decrypt with Client's Private Key(2):" + uncryaes);
+            
+            // Decrypt AES Key with Server's Public Key
+            ObjectInputStream inputStream2 = null;
+            inputStream2 = new ObjectInputStream(new FileInputStream(PUBLIC_KEY_FILE));
+            final PublicKey publicKey = (PublicKey) inputStream2.readObject();
+            final String uncryaes2 = decrypt2(uncryaes.getBytes(), publicKey);
+            System.out.println("Decrypt with Server's Public Key(1):" + uncryaes2);
+            
+            SecretKeySpec secretKeySpec = new SecretKeySpec(uncryaes2.getBytes(), "AES");
+            System.out.println("Symmetrical Key: " + secretKeySpec);
+
+        while (true) {
+            
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter Message:");
+            fromUser= keyboard.nextLine();
+            
+            byte[] plaintext = fromUser.getBytes(StandardCharsets.UTF_8);
+            
+	    
+            if (fromUser != null) {
+                
+                
+                Cipher AES_Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC"); 
+                AES_Cipher.init (Cipher.ENCRYPT_MODE, secretKeySpec);
+                byte ciphertext []= AES_Cipher.doFinal (plaintext);
+                
+                String encoded = Base64.getEncoder().encodeToString(ciphertext);
+                
+                out.println(encoded);
+                
+	    }
+             else{ break; }
+
+            fromServer = in.readLine();
+            if(fromServer!=null){
+                Cipher AES_Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
+                AES_Cipher.init (Cipher.DECRYPT_MODE, secretKeySpec);
+                
+                byte[] decoded = Base64.getDecoder().decode(fromServer);
+                byte[] plaintext2 = AES_Cipher.doFinal(decoded);
+                
+                System.out.println("Client receive :" + new String(plaintext2, StandardCharsets.UTF_8));
+                
+            }
+            else{  break; }
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        mySocket.close();
+    }
+    
+    // String to hold name of the encryption algorithm.
+    public static final String ALGORITHM2 = "RSA/None/NoPadding";
+    public static final String ALGORITHM = "RSA/ECB/PKCS1Padding";
+    //String to hold the name of the private key file.
+    public static final String PRIVATE_KEY_FILE = "C:/keys/private2.key";
+    //String to hold name of the public key file.
+    public static final String PUBLIC_KEY_FILE = "C:/keys/public3.key";
+
+    //Elegxos an uparxoun kleidia stis topothesies
+    public static boolean areKeysPresent() {
+
+      File privateKey = new File(PRIVATE_KEY_FILE);
+      File publicKey = new File(PUBLIC_KEY_FILE);
+
+      if (privateKey.exists() && publicKey.exists()) {
+        return true;
+      }
+      return false;
+    }
+
+    //Decrypt (text, key)
+    public static String decrypt(byte[] text, PrivateKey key) {
+      byte[] dectyptedText = null;
+      try {
+        // get an RSA cipher object and print the provider
+        final Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+        // decrypt the text using the private key
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        dectyptedText = cipher.doFinal(text);
+
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+
+      return new String(dectyptedText);
+    }
+
+    //Decrypt (text, key)
+    public static String decrypt2(byte[] text2, PublicKey key2) {
+      byte[] dectyptedText2 = null;
+      try {
+        // get an RSA cipher object and print the provider
+        final Cipher cipher = Cipher.getInstance(ALGORITHM2);
+
+        // decrypt the text using the public key
+        cipher.init(Cipher.DECRYPT_MODE, key2);
+        dectyptedText2 = cipher.doFinal(text2);
+        
+        
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+      
+      return new String(dectyptedText2);
+    }
+    
+    
+    
+    
+}
---
+++ src/Server.java
@@ -0,0 +1,188 @@
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class Server {
+    public static void main(String[] args) throws IOException, 
+NoSuchAlgorithmException, NoSuchProviderException, 
+NoSuchPaddingException, InvalidKeyException, 
+IllegalBlockSizeException, BadPaddingException, 
+InvalidAlgorithmParameterException, ClassNotFoundException{
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(44430);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 44430.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+				clientSocket.getInputStream()));
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine, outputLine;
+
+        System.out.println("Server run ");
+        
+        // Generate AES Key
+        KeyGenerator AES_keygen = KeyGenerator.getInstance ("AES");             
+	AES_keygen.init (256, new SecureRandom());                              
+	SecretKey AES_Key = AES_keygen.generateKey ();
+        System.out.println("1. Generate Key: " + AES_Key);
+        
+        // Convert to Byte
+        byte[] data = AES_Key.getEncoded();
+        System.out.println("2. Convert Key to Byte:" + data);
+        
+        //Encode Key
+        String aeskeyencoded = Base64.getEncoder().encodeToString(data);
+        System.out.println("3. Encode Key:" + aeskeyencoded);
+        System.out.println("3. Binary Key:" + aeskeyencoded.getBytes());
+        
+        // Check for keys
+        if (!areKeysPresent()) {
+            System.out.println("\n "
+                            + "-------------------------------------------\n"
+                            + "    Cant find Private and Public Keys!!    \n"
+                            + "       Default Folder: C:/keys/            \n"
+                            + "-------------------------------------------\n");
+            
+        }
+        
+        
+        // Encrypt AES Key with Server's Private RSA Key
+        ObjectInputStream inputStream = null;
+        inputStream = new ObjectInputStream(new FileInputStream(PRIVATE_KEY_FILE));
+        final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
+        final byte[] cryptoaeskey = encrypt(aeskeyencoded, privateKey);
+        System.out.println("4. Encrypt with Server's Private Key(1): " + cryptoaeskey);
+        
+        //Debug
+        System.out.println("Debug: " + cryptoaeskey.toString());
+        
+        // Encrypt AES Key with Client's Public RSA Key
+        ObjectInputStream inputStream2 = null;
+        inputStream2 = new ObjectInputStream(new FileInputStream(PUBLIC_KEY_FILE));
+        final PublicKey publicKey = (PublicKey) inputStream2.readObject();
+        //String cryptoaeskeystr = new String(cryptoaeskey, "UTF-8");
+        final byte[] cryptoaeskey2 = encrypt2(cryptoaeskey.toString(), publicKey);
+        System.out.println("6. Encrypt with Client's Public Key(2): " + cryptoaeskey2);
+        
+        
+        // Encode
+        String encodeaeskey = Base64.getEncoder().encodeToString(cryptoaeskey2);
+        System.out.println("7. Encode Kleidiou:" + encodeaeskey);
+        System.out.println("7. Binary Kleidiou:" + encodeaeskey.getBytes());
+        
+        // Send Encrypted AES Key
+        out.println(encodeaeskey);
+        System.out.println("8. Sending Key: " + encodeaeskey);
+        
+        while ((inputLine = in.readLine()) != null) {
+            
+            
+            Cipher AES_Cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
+            AES_Cipher.init (Cipher.DECRYPT_MODE, AES_Key);
+            byte[] decoded = Base64.getDecoder().decode(inputLine);
+            byte[] plaintext = AES_Cipher.doFinal(decoded);
+            
+            System.out.println("Server receive : " + new String(plaintext, StandardCharsets.UTF_8));
+            
+            System.out.println("type message :");
+             outputLine = stdIn.readLine();
+             byte[] plaintext2 = outputLine.getBytes(StandardCharsets.UTF_8);
+             AES_Cipher.init (Cipher.ENCRYPT_MODE, AES_Key);
+             byte ciphertext []= AES_Cipher.doFinal (plaintext2);
+             String encoded = Base64.getEncoder().encodeToString(ciphertext);
+             
+             out.println(encoded);
+        }
+        
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    
+    /**
+     * RSA Input Files & Encrypt & Decrypt
+     */
+    
+
+  // String to hold name of the encryption algorithm.
+  public static final String ALGORITHM2 = "RSA/None/NoPadding";
+  public static final String ALGORITHM = "RSA/ECB/PKCS1Padding";
+  //String to hold the name of the private key file.
+  public static final String PRIVATE_KEY_FILE = "C:/keys/private3.key";
+  //String to hold name of the public key file.
+  public static final String PUBLIC_KEY_FILE = "C:/keys/public2.key";
+    
+  //Elegxos an uparxoun kleidia stis topothesies
+  public static boolean areKeysPresent() {
+
+    File privateKey = new File(PRIVATE_KEY_FILE);
+    File publicKey = new File(PUBLIC_KEY_FILE);
+
+    if (privateKey.exists() && publicKey.exists()) {
+      return true;
+    }
+    return false;
+  }
+    
+  //Κρυπτογράφηση με Δημόσιο Κλειδί Client (text, key)
+  public static byte[] encrypt2(String text, PublicKey key) {
+    byte[] cipherText = null;
+    try {
+      // get an RSA cipher object and print the provider
+      final Cipher cipher = Cipher.getInstance(ALGORITHM);
+      
+      // encrypt the plain text using the public key
+      cipher.init(Cipher.ENCRYPT_MODE, key);
+      cipherText = cipher.doFinal(text.getBytes());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return cipherText;
+  }
+  //Κρυπτογράφηση με Ιδιωτικό Κλειδί Server (text, key)
+  public static byte[] encrypt(String text, PrivateKey key) {
+    byte[] cipherText2 = null;
+    try {
+      // get an RSA cipher object and print the provider
+      final Cipher cipher = Cipher.getInstance(ALGORITHM2);
+      
+      // encrypt the plain text using the public key
+      cipher.init(Cipher.ENCRYPT_MODE, key);
+      cipherText2 = cipher.doFinal(text.getBytes());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return cipherText2;
+  }
+
+}
